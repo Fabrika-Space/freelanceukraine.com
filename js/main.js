3eb4f5dabc056f62cee10a952cef96235a27b42a
NightNei <nei.css@gmail.com>
2016-05-13 12:35:17 +0300

    add milisecoinds

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ $(function(){
 			navigationControl: false,
 			streetViewControl: false,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-		var map = new google.maps.Map(mapCanvas, mapOptions)
+		};
+		var map = new google.maps.Map(mapCanvas, mapOptions);
 		var marker = new google.maps.Marker({
 			position: myLatlng,
 			map: map,
@@ -25,33 +25,35 @@ $(function(){
 
 	function getTimeRemaining(endtime) {
 		var t = Date.parse(endtime) - Date.parse(new Date());
+		var miliseconds = Math.floor((t / 100) % 60);
 		var seconds = Math.floor((t / 1000) % 60);
 		var minutes = Math.floor((t / 1000 / 60) % 60);
 		var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
 		var days = Math.floor(t / (1000 * 60 * 60 * 24));
 		return {
 			'total': t,
-			'days': days,
-			'hours': hours,
+			//'days': days,
+			'hours': days * 24 + hours,
 			'minutes': minutes,
-			'seconds': seconds
+			'seconds': seconds,
+			'miliseconds': miliseconds,
 		};
 	}
 
 	function initializeClock(id, endtime) {
 		var clock = document.getElementById(id);
-		var daysSpan = clock.querySelector('.days');
 		var hoursSpan = clock.querySelector('.hours');
 		var minutesSpan = clock.querySelector('.minutes');
 		var secondsSpan = clock.querySelector('.seconds');
+		var milisecondsSpan = clock.querySelector('.miliseconds');
 
 		function updateClock() {
 			var t = getTimeRemaining(endtime);
 
-			daysSpan.innerHTML = t.days;
 			hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
 			minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
 			secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+			milisecondsSpan.innerHTML = ('0' + t.miliseconds).slice(-2);
 
 			if (t.total <= 0) {
 				clearInterval(timeinterval);
@@ -59,7 +61,7 @@ $(function(){
 		}
 
 		updateClock();
-		var timeinterval = setInterval(updateClock, 1000);
+		var timeinterval = setInterval(updateClock, 10);
 	}
 
 	//new Date(year, month, day, hours, minutes, seconds, milliseconds)
